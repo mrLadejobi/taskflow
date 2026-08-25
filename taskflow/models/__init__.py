a30408afc5b0555b12afcf9cb94f824e8a994b44
mrLadejobi <ladejobioluwasaanumi@gmail.com>
2026-08-25 08:11:33 +0100

    refactor: export all models from models package

---
+++ taskflow/models/__init__.py
@@ -0,0 +1,6 @@
+"""SQLAlchemy models for TaskFlow."""
+from taskflow.models.project import Project
+from taskflow.models.task import Task, TaskPriority, TaskStatus
+from taskflow.models.user import User
+
+__all__ = ["User", "Project", "Task", "TaskStatus", "TaskPriority"]
